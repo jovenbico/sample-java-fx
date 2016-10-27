baae1c0314e245df2b1bf3f59f3852a6550eec16
Joven Frankie Bico <jfbico@xmnt.net>
2016-10-27 20:56:51 +0800

    using PropertyBuilder

--- src/com/bicjo/sample/login/UsernamePasswordProperties.java
+++ src/com/bicjo/sample/login/UsernamePasswordProperties.java
@@ -2,25 +2,26 @@ package com.bicjo.sample.login;
 
 import com.bicjo.sample.login.object.UsernamePassword;
 
-import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.property.adapter.JavaBeanStringPropertyBuilder;
 
 public class UsernamePasswordProperties {
 
 	private UsernamePassword usernamePassword = new UsernamePassword();
 
-	private StringProperty usernameProperty = new SimpleStringProperty(this, "username") {
-		public void set(String newValue) {
-			super.set(newValue);
-			usernamePassword.setUsername(newValue);
-		};
-	};
-	private StringProperty passwordProperty = new SimpleStringProperty(this, "password") {
-		public void set(String newValue) {
-			super.set(newValue);
-			usernamePassword.setPassword(newValue);
-		};
-	};
+	private StringProperty usernameProperty;
+	private StringProperty passwordProperty;
+
+	public UsernamePasswordProperties() {
+		try {
+
+			usernameProperty = JavaBeanStringPropertyBuilder.create().bean(usernamePassword).name("username").build();
+			passwordProperty = JavaBeanStringPropertyBuilder.create().bean(usernamePassword).name("password").build();
+
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public UsernamePassword getUsernamePassword() {
 		return usernamePassword;
